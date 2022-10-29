518d710ed79ef316923d4a5219b78a1e65db9111
mishal2509 <mishal.ahmed@adglobe.co.jp>
2022-10-29 21:18:13 +0900

    add: react with typescript and tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
+import "./index.css";
 const App = (): JSX.Element => {
-  return <div>hello world</div>;
+  return <div className="text-3xl font-bold underline">hello world</div>;
 };
 
 export default App;
