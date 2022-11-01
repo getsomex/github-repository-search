467da0a48158b7108c42b0d01ccf62dd6991da35
mishal2509 <mishal.ahmed@adglobe.co.jp>
2022-11-01 19:03:22 +0900

    api constants

---
+++ src/constant/constant.ts
@@ -0,0 +1,10 @@
+/**
+ * GITHUB API
+ */
+export const GITHUB_API = {
+  INITAL_PAGE: 1,
+  LIMIT: 10,
+  API_THROTTLE: 1000,
+  TOTAL_COUNT: 1000, // NOTE: Current Github api only allows for 1000 repositories
+  BASE_URL: 'https://api.github.com/search/repositories',
+};
