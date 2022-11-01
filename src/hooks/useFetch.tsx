1f20325ff4fdd69150e4d26eced32edac6785e43
mishal2509 <mishal.ahmed@adglobe.co.jp>
2022-11-01 19:01:07 +0900

    useFetch hook

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,25 +1,27 @@
 import { useState, useEffect } from 'react';
-
 type UseFetch<T> = {
   loading: boolean;
   data?: T;
   fetchData: (url: string) => Promise<void>;
 };
-export const useFetch = <T,>(url: string, q?: string): UseFetch<T> => {
+/**
+ * Fetch Hoo
+ * @param {string} url
+ * @returns UseFetch<T>
+ */
+export const useFetch = <T,>(url: string): UseFetch<T> => {
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState<T>();
 
   const fetchData = async (query?: string): Promise<void> => {
     setLoading(true);
-    const response = await fetch(`${url}?${(query || q) ?? ''}`, {
+    const response = await fetch(`${url}${query ? '?' + query : ''}`, {
       method: 'GET',
       headers: {
         Accept: 'application/json',
       },
     });
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     const result = (await response.json()) as unknown as T;
-    console.log(result);
     setData(result);
     setLoading(false);
   };
